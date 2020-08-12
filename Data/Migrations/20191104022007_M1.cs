504983eb0af9cebf5561e8c3410ddd1b5e52fe75
Medhat Elmasry <medhat@elmasry.ca>
2020-08-12 12:57:36 -0700

    1st commit

---
+++ Data/Migrations/20191104022007_M1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolAPI.Data.Migrations
+{
+    public partial class M1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    School = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "StudentId", "FirstName", "LastName", "School" },
+                values: new object[,]
+                {
+                    { "2f776a56-b106-4d9a-90fc-147d80d1cf65", "Jane", "Smith", "Medicine" },
+                    { "066ac05d-628f-477e-9471-071dc4bca2a0", "John", "Fisher", "Engineering" },
+                    { "e5349cc2-2401-43ea-bc62-34f8192349c4", "Pamela", "Baker", "Food Science" },
+                    { "582f6939-6924-41e2-a304-2b329a8b0e01", "Peter", "Taylor", "Mining" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolAPI.Models
+{
+    public class Student
+    {
+        public string StudentId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string School { get; set; }
+    }
+
+}
